36b1427bf85d412cac1fe8a8aecd09405fc43644
unknown <dylanbram354@gmail.com>
2021-07-09 12:24:35 -0400

    Added initial CS files

---
+++ Gesture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Gesture
+    {
+        public string name;
+        public string beats;
+        public string attackWords;
+
+        public Gesture(string name, string beats, string attackWords)
+        {
+            this.name = name;
+            this.beats = beats;
+            this.attackWords = attackWords
+        }
+    }
+}
